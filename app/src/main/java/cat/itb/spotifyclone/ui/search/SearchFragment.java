c1ef4402bcaca0ac4d24281a649ca09eb416ceb4
dcumelles7e3 <a>
2021-02-12 17:58:58 +0100

    buscar

---
+++ app/src/main/java/cat/itb/spotifyclone/ui/search/SearchFragment.java
@@ -0,0 +1,25 @@
+package cat.itb.spotifyclone.ui.search;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProviders;
+
+import cat.itb.spotifyclone.R;
+
+public class SearchFragment extends Fragment {
+
+    private SearchViewModel searchViewModel;
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        searchViewModel = ViewModelProviders.of(this).get(SearchViewModel.class);
+        View v = inflater.inflate(R.layout.fragment_search, container, false);
+
+        return v;
+    }
+}
--- app/src/main/java/cat/itb/spotifyclone/ui/search/SearchViewModel.java
+++ app/src/main/java/cat/itb/spotifyclone/ui/search/SearchViewModel.java
@@ -1,14 +1,14 @@
-package cat.itb.spotifyclone.ui.dashboard;
+package cat.itb.spotifyclone.ui.search;
 
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
-public class DashboardViewModel extends ViewModel {
+public class SearchViewModel extends ViewModel {
 
     private MutableLiveData<String> mText;
 
-    public DashboardViewModel() {
+    public SearchViewModel() {
         mText = new MutableLiveData<>();
         mText.setValue("This is dashboard fragment");
     }
