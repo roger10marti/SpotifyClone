04f73b92866fb8a647126452c7a5ecbe6c4063b9
dcumelles7e3 <a>
2021-02-19 18:14:13 +0100

    loginactivity

---
+++ app/src/main/java/cat/itb/spotifyclone/LoginActivity.java
@@ -0,0 +1,30 @@
+package cat.itb.spotifyclone;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+public class LoginActivity extends AppCompatActivity {
+
+    private Button loginbutton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        loginbutton = findViewById(R.id.loginbutton);
+
+        loginbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
