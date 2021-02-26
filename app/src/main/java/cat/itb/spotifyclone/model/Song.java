fa249b2da8d95600a580b588fe3b7ccb010ec1d0
sjo <e>
2021-02-26 16:39:34 +0100

    ekisde

---
+++ app/src/main/java/cat/itb/spotifyclone/model/Song.java
@@ -0,0 +1,25 @@
+package cat.itb.spotifyclone.model;
+
+public class Song {
+    private String title;
+    private String album;
+
+    public Song() {
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAlbum() {
+        return album;
+    }
+
+    public void setAlbum(String album) {
+        this.album = album;
+    }
+}
---
+++ app/src/main/java/cat/itb/spotifyclone/ui/secondaryfragments/FragmentAlbumList.java
@@ -0,0 +1,34 @@
+package cat.itb.spotifyclone.ui.secondaryfragments;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import cat.itb.spotifyclone.R;
+
+public class FragmentAlbumList extends Fragment {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_album_list, container, false);
+
+
+
+
+
+
+
+        return v;
+    }
+}
