207e769de05f7baee1f1a8401d303b105430e91a
dcumelles7e3 <a>
2021-03-02 19:08:14 +0100

    player icons

--- app/src/main/java/cat/itb/spotifyclone/PlayerActivity.java
+++ app/src/main/java/cat/itb/spotifyclone/PlayerActivity.java
@@ -1,13 +1,20 @@
 package cat.itb.spotifyclone;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
+import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 public class PlayerActivity extends AppCompatActivity {
 
+    ImageView b_play;
     TextView songTitleText;
+    boolean playing = false;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -15,5 +22,20 @@ public class PlayerActivity extends AppCompatActivity {
 
         songTitleText = findViewById(R.id.songtitle);
         songTitleText.setSelected(true);
+
+        b_play = findViewById(R.id.b_play);
+        b_play.setOnClickListener(new View.OnClickListener() {
+            @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+            @Override
+            public void onClick(View v) {
+                if (playing){
+                    b_play.setImageDrawable(ContextCompat.getDrawable(getApplicationContext(),R.drawable.ic_pause));
+                    playing=false;
+                }else {
+                    b_play.setImageDrawable(ContextCompat.getDrawable(getApplicationContext(),R.drawable.ic_play));
+                    playing=true;
+                }
+            }
+        });
     }
 }
