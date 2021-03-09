75e879f267a1bf87468dc2df2a243813903fc2bf
sjo <e>
2021-03-09 19:01:12 +0100

    Sign up and sign in with firebase authentication

--- app/src/main/java/cat/itb/spotifyclone/LoginActivity.java
+++ app/src/main/java/cat/itb/spotifyclone/LoginActivity.java
@@ -1,5 +1,7 @@
 package cat.itb.spotifyclone;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -8,11 +10,23 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.button.MaterialButton;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class LoginActivity extends AppCompatActivity {
 
     private Button loginbutton;
+    private MaterialButton loginGoogle;
+    private FirebaseAuth mAuth;
     private TextView signUp;
+    private TextInputEditText etPass, etEmail;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,14 +34,30 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+        mAuth = FirebaseAuth.getInstance();
         loginbutton = findViewById(R.id.loginbutton);
         signUp = findViewById(R.id.registerText);
+        etPass = findViewById(R.id.etpass);
+        etEmail = findViewById(R.id.etloginUser);
 
         loginbutton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(LoginActivity.this, MainActivity.class);
-                startActivity(intent);
+                if (!etEmail.getText().toString().isEmpty() && !etPass.getText().toString().isEmpty()){
+                    mAuth.signInWithEmailAndPassword(etEmail.getText().toString(),etPass.getText().toString())
+                            .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                                @Override
+                                public void onComplete(@NonNull Task<AuthResult> task) {
+                                    if (task.isSuccessful()) {
+                                        Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                                        startActivity(intent);
+                                    } else {
+                                        showAlert("Esta cuenta de usuario no existe");
+                                    }
+                                }
+
+                    });
+                }
             }
         });
 
@@ -39,5 +69,14 @@ public class LoginActivity extends AppCompatActivity {
             }
         });
 
+    }
+
+    public void showAlert(String text) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Error");
+        builder.setMessage(text);
+        builder.setPositiveButton("Aceptar",null);
+        AlertDialog al = builder.create();
+        al.show();
     }
 }
--- app/src/main/java/cat/itb/spotifyclone/RegisterActivity.java
+++ app/src/main/java/cat/itb/spotifyclone/RegisterActivity.java
@@ -1,5 +1,7 @@
 package cat.itb.spotifyclone;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -8,24 +10,64 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthEmailException;
+import com.google.firebase.auth.FirebaseAuthUserCollisionException;
+import com.google.firebase.auth.FirebaseAuthWeakPasswordException;
+
 public class RegisterActivity extends AppCompatActivity {
 
     private Button signUpButton;
     private TextView alreadyHaveAcc;
+    private TextInputEditText etPass, etEmail, etRepeatPassword, etUsername;
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
 
+        etPass = findViewById(R.id.etpass);
+        etEmail = findViewById(R.id.etloginUser);
+        etRepeatPassword = findViewById(R.id.etrepeatpass);
+        etUsername = findViewById(R.id.etusername);
+
         signUpButton = findViewById(R.id.signupButton);
         alreadyHaveAcc = findViewById(R.id.alreadyAccount);
+        mAuth = FirebaseAuth.getInstance();
 
         signUpButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(RegisterActivity.this, MainActivity.class);
-                startActivity(intent);
+                if (!etEmail.getText().toString().isEmpty() && !etPass.getText().toString().isEmpty()
+                        && !etRepeatPassword.getText().toString().isEmpty() && !etUsername.getText().toString().isEmpty()
+                        && etPass.getText().toString().equals(etRepeatPassword.getText().toString())) {
+                    mAuth.createUserWithEmailAndPassword(etEmail.getText().toString(), etPass.getText().toString())
+                            .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                                @Override
+                                public void onComplete(@NonNull Task<AuthResult> task) {
+                                    if (task.isSuccessful()) {
+                                        Intent intent = new Intent(RegisterActivity.this, MainActivity.class);
+                                        startActivity(intent);
+                                    } else {
+                                        Exception e = task.getException();
+                                        if (e instanceof FirebaseAuthUserCollisionException) {
+                                            showAlert("Esta cuenta email ya pertenece a una cuenta");
+                                        } else if (e instanceof FirebaseAuthEmailException) {
+                                            showAlert("Email con formato incorrecto");
+                                        } else if (e instanceof FirebaseAuthWeakPasswordException) {
+                                            showAlert("Esta contraseña es denasiado corta");
+                                        }
+                                    }
+                                }
+                            });
+                }
             }
         });
 
@@ -36,5 +78,14 @@ public class RegisterActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+    }
+
+    public void showAlert(String text) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Error");
+        builder.setMessage(text);
+        builder.setPositiveButton("Aceptar", null);
+        AlertDialog al = builder.create();
+        al.show();
     }
 }
