dc3bae29da5b286cfa3e893e182cc8b56c1323cd
sjo <e>
2021-03-17 18:13:06 +0100

    new fragment searching

---
+++ app/src/main/java/cat/itb/spotifyclone/ui/search/SerachingFragment.java
@@ -0,0 +1,30 @@
+package cat.itb.spotifyclone.ui.search;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import cat.itb.spotifyclone.R;
+
+public class SerachingFragment extends Fragment {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_seraching, container, false);
+
+
+
+        return v;
+    }
+}
