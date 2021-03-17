69f54b5c4d2eb0e727417eafb16a3f5e86c67ff5
sjo <a>
2021-03-17 18:13:51 +0100

    pusheito

--- app/src/main/java/cat/itb/spotifyclone/ui/home/HomeViewModel.java
+++ app/src/main/java/cat/itb/spotifyclone/ui/home/HomeViewModel.java
@@ -1,7 +1,5 @@
 package cat.itb.spotifyclone.ui.home;
 
-import androidx.lifecycle.LiveData;
-import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
 import java.util.ArrayList;
@@ -19,16 +17,27 @@ public class HomeViewModel extends ViewModel {
 
     public HomeViewModel() {
 
-        String busqueda = "roger";
-        List<Datum> call = ApiHelper.lanzarPeticion("https://api.deezer.com/search/?q="+busqueda+"&index=0&limit=1&output=json");
-        String title = call.get(0).getAlbum().getTitle();
-        String caratula = call.get(0).getAlbum().getCoverMedium();
-        Albumold a1 = new Albumold(title,caratula);
-        Albumold a2 = new Albumold("Segundo",caratula);
-        Albumold a3 = new Albumold("Tercero",caratula);
-        Albumold a4 = new Albumold("Cuarto",caratula);
-        Albumold a5 = new Albumold("Quinto",caratula);
-        Albumold a6 = new Albumold("Sexto",caratula);
+        String busqueda = "bad_bunny";
+        List<Datum> call = ApiHelper.lanzarPeticion("https://api.deezer.com/search/?q="+busqueda+"&index=0&limit=6&output=json");
+
+        String title1 = call.get(0).getAlbum().getTitle();
+        String caratula1 = call.get(0).getAlbum().getCoverMedium();
+        String title = call.get(1).getAlbum().getTitle();
+        String caratula = call.get(1).getAlbum().getCoverMedium();
+        String title2 = call.get(2).getAlbum().getTitle();
+        String caratula2 = call.get(2).getAlbum().getCoverMedium();
+        String title3 = call.get(3).getAlbum().getTitle();
+        String caratula3 = call.get(3).getAlbum().getCoverMedium();
+        String title4 = call.get(4).getAlbum().getTitle();
+        String caratula4 = call.get(4).getAlbum().getCoverMedium();
+        String title5 = call.get(5).getAlbum().getTitle();
+        String caratula5 = call.get(5).getAlbum().getCoverMedium();
+        Albumold a1 = new Albumold(title1,caratula1);
+        Albumold a2 = new Albumold(title,caratula);
+        Albumold a3 = new Albumold(title2,caratula2);
+        Albumold a4 = new Albumold(title3,caratula3);
+        Albumold a5 = new Albumold(title4,caratula4);
+        Albumold a6 = new Albumold(title5,caratula5);
 
         albumes.add(a1);
         albumes.add(a2);
@@ -36,10 +45,6 @@ public class HomeViewModel extends ViewModel {
         albumes.add(a4);
         albumes.add(a5);
         albumes.add(a6);
-    }
-
-    public int random() {
-        return (int) (Math.random()*8+1);
     }
 
     public List<Albumold> getAlbumes() {
