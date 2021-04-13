9ec8d5ffc72f761d83fedc94d4639907017953a5
sjo <e>
2021-04-13 19:08:21 +0200

    fragment favourite songs

---
+++ app/src/main/java/cat/itb/spotifyclone/ui/FavouriteSongs.java
@@ -0,0 +1,33 @@
+package cat.itb.spotifyclone.ui;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import cat.itb.spotifyclone.R;
+
+
+public class FavouriteSongs extends Fragment {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_favourite_songs, container, false);
+
+
+
+
+
+        return v;
+    }
+}
